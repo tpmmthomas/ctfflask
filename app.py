de3935e3c80c03a70ec6ed171e1851e123ba8a2b
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-11-12 23:17:13 +0800

    update

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, request
+import logging
 
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 app = Flask(__name__)
-
+logger = logging.getLogger(__name__)
 latest = ""
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def home():
-    global latest
-    latest += str(request.args)
-    return "Hello, World!" + str(latest)
+    logger.info(str(request.args))
+    # Do what you need if post
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
