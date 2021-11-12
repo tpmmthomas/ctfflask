60b255480f90f20579a17ced6e8fe3303b62b363
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-11-12 21:21:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    # print(request.args)
-    return "Hello, World!"  # + str(request.args)
+    print(request.args)
+    return "Hello, World!" + str(request.args)
 
 
 if __name__ == "__main__":
